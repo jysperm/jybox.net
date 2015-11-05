2f385323352c5979f2909e12d642945c2cc69963
jysperm <jysperm@gmail.com>
2015-11-06 00:03:24 +0800

    Load blog and tweets from jysperm.me

---
+++ themes/jybox/scripts/filters.js
@@ -0,0 +1,20 @@
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
+
+hexo.extend.filter.register('before_generate', function() {
+  return request({
+    url: 'https://jysperm.me/blogs.json',
+    strictSSL: false
+  }).then(function(res) {
+    hexo.locals.set('blogs', JSON.parse(res.body));
+  });
+});
+
+hexo.extend.filter.register('before_generate', function() {
+  return request({
+    url: 'https://jysperm.me/tweets.json',
+    strictSSL: false
+  }).then(function(res) {
+    hexo.locals.set('tweets', JSON.parse(res.body));
+  });
+});
