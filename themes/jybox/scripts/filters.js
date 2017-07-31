3f85840c27f8ac9a4689b69146ad6b2014487e4a
jysperm <jysperm@gmail.com>
2017-07-31 21:36:43 +0800

    :sparkles: Some about yellowbud

--- themes/jybox/scripts/filters.js
+++ themes/jybox/scripts/filters.js
@@ -8,11 +8,3 @@ hexo.extend.filter.register('before_generate', function() {
     hexo.locals.set('blogs', JSON.parse(res.body));
   });
 });
-
-hexo.extend.filter.register('before_generate', function() {
-  return request({
-    url: 'https://jysperm.me/tweets.json',
-  }).then(function(res) {
-    hexo.locals.set('tweets', JSON.parse(res.body));
-  });
-});
